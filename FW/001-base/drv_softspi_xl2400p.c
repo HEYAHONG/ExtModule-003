be6e272884dca7fc1b515c1f4103090b95992b64
HEYAHONG <2229388563@qq.com>
2025-05-30 11:22:56 +0800

    添加XL2400P软SPI驱动

---
+++ FW/001-base/drv_softspi_xl2400p.c
@@ -0,0 +1,159 @@
+#include "main.h"
+#include "hbox.h"
+
+#define CSN_Low()                       HAL_GPIO_WritePin(GPIOA, GPIO_PIN_1, GPIO_PIN_RESET);
+#define CSN_High()                      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_1, GPIO_PIN_SET);
+
+#define SCK_High()                      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0, GPIO_PIN_SET);
+#define SCK_Low()                       HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0, GPIO_PIN_RESET);
+
+#define SPI_MOSI_High()                 HAL_GPIO_WritePin(GPIOF, GPIO_PIN_3, GPIO_PIN_SET);
+#define SPI_MOSI_Low()                  HAL_GPIO_WritePin(GPIOF, GPIO_PIN_3, GPIO_PIN_RESET);
+
+#define Read_SPI_MISO()                 HAL_GPIO_ReadPin(GPIOF,GPIO_PIN_1)
+
+static void hsoftspi_xl2400p_lowlevel_init()
+{
+    /*使能模块时钟*/
+    __HAL_RCC_GPIOF_CLK_ENABLE();//PF
+    __HAL_RCC_GPIOA_CLK_ENABLE();//PA
+
+    GPIO_InitTypeDef   GPIO_InitStruct;
+
+    GPIO_InitStruct.Pin   = GPIO_PIN_1;
+    GPIO_InitStruct.Mode  = GPIO_MODE_INPUT;
+    GPIO_InitStruct.Pull  = GPIO_PULLUP;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+
+    HAL_GPIO_Init(GPIOF, &GPIO_InitStruct);
+
+    GPIO_InitStruct.Pin   = GPIO_PIN_3;
+    GPIO_InitStruct.Mode  = GPIO_MODE_OUTPUT_PP;
+    GPIO_InitStruct.Pull  = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+
+    HAL_GPIO_Init(GPIOF, &GPIO_InitStruct);
+
+    GPIO_InitStruct.Pin   = GPIO_PIN_0|GPIO_PIN_1;
+    GPIO_InitStruct.Mode  = GPIO_MODE_OUTPUT_PP;
+    GPIO_InitStruct.Pull  = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+
+    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+    CSN_High();
+    SCK_Low();
+
+}
+
+#ifdef HRUNTIME_USING_INIT_SECTION
+void  hsoftspi_xl2400p_init(const hruntime_function_t *func)
+{
+    hsoftspi_xl2400p_lowlevel_init();
+}
+HRUNTIME_INIT_EXPORT(softspi_xl2400p,0,hsoftspi_xl2400p_init,NULL);
+#endif
+#ifdef HRUNTIME_USING_LOOP_SECTION
+void  hsoftspi_xl2400p_loop(const hruntime_function_t *func)
+{
+
+}
+HRUNTIME_LOOP_EXPORT(softspi_xl2400p,0,hsoftspi_xl2400p_loop,NULL);
+#endif
+
+static void hsoftspi_xl2400p_write_byte(uint8_t buff)
+{
+    uint8_t i = 0;
+
+    for(i = 0; i < 8; i++)
+    {
+        SCK_Low();
+
+        if(buff & 0x80)
+        {
+            SPI_MOSI_High();
+        }
+        else
+        {
+            SPI_MOSI_Low();
+        }
+
+        buff = buff << 1;
+
+        SCK_High();
+    }
+    SPI_MOSI_High();
+    SCK_Low();
+}
+
+
+static uint8_t hsoftspi_xl2400p_read_byte(void)
+{
+
+    uint8_t  buff = 0;
+    uint8_t  i = 0;
+
+    for(i = 0; i < 8; i++)
+    {
+        SCK_Low();
+        buff = buff << 1;
+        SCK_High();
+        if(Read_SPI_MISO())
+        {
+            buff |= 0x01;
+        }
+    }
+    SCK_Low();
+
+    return buff;
+}
+
+
+void hsoftspi_xl2400p_write_register(uint8_t RF_Reg,uint8_t W_Data)
+{
+    CSN_Low();//拉低片选
+    hsoftspi_xl2400p_write_byte(RF_Reg);//写入地址
+    hsoftspi_xl2400p_write_byte(W_Data);//写入数据
+    CSN_High();//写入完成拉高片选
+}
+
+
+uint8_t hsoftspi_xl2400p_read_register(uint8_t RF_Reg)
+{
+    uint8_t rTemp =0;
+    CSN_Low();
+    hsoftspi_xl2400p_write_byte(RF_Reg);//写入地址+读取指令
+    rTemp = hsoftspi_xl2400p_read_byte();//读取数据
+    CSN_High();
+    return rTemp;
+}
+
+
+void hsoftspi_xl2400p_write_register_buffer(uint8_t RF_Reg, uint8_t *pBuff, uint8_t Len)
+{
+    uint8_t rTemp = 0;
+    uint8_t i = 0;
+    CSN_Low();
+    hsoftspi_xl2400p_write_byte(RF_Reg);
+    for(i = 0 ; i < Len ; i++)
+    {
+        rTemp = pBuff[i];
+        hsoftspi_xl2400p_write_byte(rTemp);
+    }
+    CSN_High();
+}
+
+
+void hsoftspi_xl2400p_read_register_buffer(uint8_t RF_Reg, uint8_t *pBuff, uint8_t Len)
+{
+    uint8_t i=0;
+    CSN_Low();
+    hsoftspi_xl2400p_write_byte(RF_Reg);
+    for(i = 0 ; i < Len ; i++)
+    {
+        pBuff[i] = hsoftspi_xl2400p_read_byte();
+    }
+    CSN_High();
+}
+
+
---
+++ FW/001-base/drv_softspi_xl2400p.h
@@ -0,0 +1,67 @@
+#ifndef __DRV_SOFTSPI_XL2400P_H__
+#define __DRV_SOFTSPI_XL2400P_H__
+#include "stdint.h"
+#include "stdbool.h"
+#include "stdlib.h"
+
+void hsoftspi_xl2400p_write_register(uint8_t RF_Reg,uint8_t W_Data);
+uint8_t hsoftspi_xl2400p_read_register(uint8_t RF_Reg);
+void hsoftspi_xl2400p_write_register_buffer(uint8_t RF_Reg, uint8_t *pBuff, uint8_t Len);
+void hsoftspi_xl2400p_read_register_buffer(uint8_t RF_Reg, uint8_t *pBuff, uint8_t Len);
+
+
+/*********************************************************************/
+/************************* 寄存器地址定义 *****************************/
+/*********************************************************************/
+
+#define XL2400P_CFG_TOP                         (0X00)
+#define XL2400P_EN_AA                           (0X01)
+#define XL2400P_EN_RXADDR                       (0X02)
+#define XL2400P_SETUP_AW                        (0X03)
+#define XL2400P_SETUP_RETR                      (0X04)
+#define XL2400P_RF_CH                           (0X05)
+#define XL2400P_RF_SETUP                        (0X06)
+#define XL2400P_RF_STATUS                       (0X07)
+#define XL2400P_OBSERVE_TX                      (0X08)
+#define XL2400P_RSSI                            (0X09)
+#define XL2400P_RX_ADDR_P0                      (0X0A)
+#define XL2400P_RX_ADDR_P1                      (0X0B)
+#define XL2400P_RX_ADDR_P2                      (0X2)        //存储在RX_ADDR_P2TOP5
+#define XL2400P_RX_ADDR_P3                      (0X3)        //存储在RX_ADDR_P2TOP5
+#define XL2400P_RX_ADDR_P4                      (0X4)        //存储在RX_ADDR_P2TOP5
+#define XL2400P_RX_ADDR_P5                      (0X5)        //存储在RX_ADDR_P2TOP5
+#define XL2400P_RX_ADDR_P2TOP5                  (0X0C)
+#define XL2400P_BER_RESULT                      (0X0D)
+#define XL2400P_AGC_SETTING                     (0X0E)
+#define XL2400P_PGA_SETTING                     (0X0F)
+#define XL2400P_TX_ADDR                         (0X10)
+#define XL2400P_RX_PW_PX                        (0X11)
+#define XL2400P_ANALOG_CFG0                     (0X12)
+#define XL2400P_ANALOG_CFG1                     (0X13)
+#define XL2400P_ANALOG_CFG2                     (0X14)
+#define XL2400P_ANALOG_CFG3                     (0X15)
+#define XL2400P_STATUS_FIFO                     (0X17)
+#define XL2400P_RSSIREC                         (0X18)
+#define XL2400P_TXPROC_CFG                      (0X19)
+#define XL2400P_RXPROC_CFG                      (0X1A)
+#define XL2400P_DYNPD                           (0X1C)
+#define XL2400P_FEATURE                         (0X1D)
+#define XL2400P_RAMP_CFG                        (0X1E)
+
+/*********************************************************************/
+/*************************** 操作指令定义 *****************************/
+/*********************************************************************/
+
+#define XL2400P_R_REGISTER                      0x00 //读寄存器指令                                     
+#define XL2400P_W_REGISTER                      0x20 //写寄存器指令
+#define XL2400P_R_RX_PLOAD                      0x61 //读接收数据指令
+#define XL2400P_W_TX_PLOAD                      0xA0 //写发射数据指令
+#define XL2400P_FLUSH_TX                        0xE1 //清空TX-FIFO指令   
+#define XL2400P_FLUSH_RX                        0xE2 //清空RX-FIFO指令 
+#define XL2400P_R_RX_PL_WID                     0x60 //读RX-FIFO数据长度指令
+#define XL2400P_W_ACK_PLOAD                     0xA8
+#define XL2400P_W_TX_PLOAD_NOACK                0xB0
+#define XL2400P_CMD_NOP                         0xFF //空操作
+
+
+#endif
